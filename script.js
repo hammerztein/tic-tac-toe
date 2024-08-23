f735e30cb9120f5cc7473348128dfa743e84c442
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-08-23 16:02:17 +0300

    Half made feature for game UI.

--- script.js
+++ script.js
@@ -86,9 +86,11 @@ const GameController = (() => {
 	};
 
 	const switchPlayer = () => {
+		console.log(activePlayer.getTicker(), activePlayer.getName());
 		activePlayer === playerOne
 			? (activePlayer = playerTwo)
 			: (activePlayer = playerOne);
+		console.log(activePlayer.getTicker(), activePlayer.getName());
 	};
 
 	const playTurn = (cell) => {
@@ -98,37 +100,19 @@ const GameController = (() => {
 		console.log(`It's ${activePlayer.getName()}s turn`);
 		// Move player to desired cell
 		Gameboard.movePlayer(activePlayer.getTicker(), cell);
-		// Print board to the console
-		prindBoard();
-		// Increment round
-		round++;
 		// Check for winner
-		checkWinner();
-		// If no winner found switch players
+		if (checkWinner()) {
+			round++;
+			isGameActive = false;
+			return;
+		}
 		switchPlayer();
-	};
-
-	// Helper method to help visualize game board
-	const prindBoard = () => {
-		const board = Gameboard.getBoard();
-		console.log(`
-		${board[0] || ' '} | ${board[1] || ' '} | ${board[2] || ' '}
-		---|---|---
-		${board[3] || ' '} | ${board[4] || ' '} | ${board[5] || ' '}
-		---|---|---
-		${board[6] || ' '} | ${board[7] || ' '} | ${board[8] || ' '}
-		`);
 	};
 
 	const checkWinner = () => {
 		// If board has less than non-empty values there can not be a winner
-		if (Gameboard.getBoard().filter((cell) => cell !== null).length < 5) return;
-		// If there are no more empty cells it's a tie
-		if (Gameboard.getBoard().filter((cell) => cell !== null).length === 9) {
-			winnerText = "It's a tie!";
-			printWinner();
-			return;
-		}
+		if (Gameboard.getBoard().filter((cell) => cell !== null).length < 5)
+			return false;
 
 		// Find active players positions in the game board
 		const activePlayerPositions = findPlayerPositions(activePlayer.getTicker());
@@ -142,18 +126,15 @@ const GameController = (() => {
 				activePlayer.addScore();
 				// Set winner text
 				winnerText = `Winner is ${activePlayer.getName()}`;
-				// print winner
-				printWinner();
+				return true;
 			}
 		}
-	};
-
-	const printWinner = () => {
-		console.log(winnerText);
-		console.log(
-			`${playerOne.getName()} : ${playerOne.getScore()} | ${playerTwo.getName()} : ${playerTwo.getScore()}`,
-		);
-		isGameActive = false;
+		// If there are no more empty cells it's a tie
+		if (Gameboard.getBoard().filter((cell) => cell !== null).length === 9) {
+			winnerText = "It's a tie!";
+			return true;
+		}
+		return false;
 	};
 
 	const findPlayerPositions = (playerTicker) => {
@@ -195,6 +176,7 @@ const GameController = (() => {
 			activePlayer,
 			round,
 			isGameActive,
+			winnerText,
 		};
 	};
 
@@ -208,11 +190,29 @@ const GameController = (() => {
 })();
 
 const DisplayController = (() => {
-	// Display modal on the first render of the screen
-	const modal = document.querySelector('dialog').showModal();
+	// Cache all DOM elements
+	const domElements = {
+		modal: {
+			container: document.querySelector('dialog'),
+			playerOneInput: document.querySelector('dialog #player-one'),
+			playerTwoInput: document.querySelector('dialog #player-two'),
+			startGameBtn: document.querySelector('dialog form button'),
+		},
+		game: {
+			board: document.querySelector('#game'),
+			roundText: document.querySelector('#round-number'),
+			playerOneName: document.querySelector('#player-one-name'),
+			playerOneScore: document.querySelector('#player-one-score'),
+			playerTwoName: document.querySelector('#player-two-name'),
+			playerTwoScore: document.querySelector('#player-two-score'),
+			gameStatus: document.querySelector('#game-status'),
+			nextRoundBtn: document.querySelector('#next-round'),
+			restartGameBtn: document.querySelector('#restart-game'),
+		},
+	};
+
 	// Draw a game board
 	const drawBoard = () => {
-		const gameboardEl = document.querySelector('#game');
 		// Create empty board
 		Gameboard.createBoard();
 		// Fill UI with cells
@@ -220,9 +220,114 @@ const DisplayController = (() => {
 			const cellEl = document.createElement('button');
 			cellEl.className = 'cell';
 			cellEl.dataset.position = index;
-			gameboardEl.appendChild(cellEl);
+			cellEl.removeAttribute('disabled');
+			domElements.game.board.appendChild(cellEl);
 		});
 	};
 
-	drawBoard();
+	const openModal = () => {
+		// Open modal
+		domElements.modal.container.showModal();
+		// Gather inputs
+		domElements.modal.startGameBtn.addEventListener('click', initializeGame);
+	};
+
+	// Set player names gathered from modal
+	const initializeGame = (e) => {
+		e.preventDefault();
+		if (
+			!domElements.modal.playerOneInput.value ||
+			!domElements.modal.playerTwoInput.value
+		) {
+			return;
+		}
+		// Set player names
+		domElements.game.playerOneName.textContent =
+			domElements.modal.playerOneInput.value;
+		domElements.game.playerTwoName.textContent =
+			domElements.modal.playerTwoInput.value;
+
+		// Set game status
+		domElements.game.gameStatus.textContent = '';
+		// Set player scores
+		domElements.game.playerOneScore.textContent = 0;
+		domElements.game.playerTwoScore.textContent = 0;
+		// Set round
+		domElements.game.roundText.textContent = 1;
+		// Empty player inputs
+		domElements.modal.playerOneInput.value = '';
+		domElements.modal.playerTwoInput.value = '';
+
+		domElements.modal.container.close();
+		startGame();
+	};
+
+	// Start the game and gather player names
+	const startGame = () => {
+		drawBoard();
+		// Start game
+		GameController.startGame(
+			domElements.game.playerOneName.textContent,
+			domElements.game.playerTwoName.textContent,
+		);
+		// Attach board event listener
+		domElements.game.board.addEventListener('click', playGame);
+	};
+
+	const playGame = (e) => {
+		// Dont play if game is not active
+		if (!GameController.getGameInfo().isGameActive) {
+			return;
+		}
+		const cell = e.target;
+		if (!cell.matches('.cell')) return;
+		if (cell.textContent !== '') return;
+		const position = cell.dataset.position;
+		cell.textContent = GameController.getGameInfo().activePlayer.getTicker();
+		GameController.playTurn(position);
+		// If winner text is true there is a winner
+		if (GameController.getGameInfo().winnerText) {
+			updateRoundWinUI();
+		}
+	};
+
+	/*
+		Update UI
+			Get game info and update UI elements
+	*/
+	const updateRoundWinUI = () => {
+		domElements.game.playerOneScore.textContent =
+			GameController.getGameInfo().activePlayer.getScore();
+		domElements.game.gameStatus.textContent =
+			GameController.getGameInfo().winnerText;
+	};
+
+	/*
+		Handle event
+			Play a round of a game
+				Use dataset-position as cell to play round
+			Update UI
+	*/
+
+	/*
+		Round end
+			Display winner text and update score for winner 
+			Disable all cells
+	*/
+
+	/*
+		Play next round
+			Attach event listener that will play next round
+			Redraw board
+	*/
+
+	/*
+		Restart game
+			Get user input via modal
+			Close modal
+			Call reset game
+			Call draw board
+	*/
+
+	openModal();
 })();
